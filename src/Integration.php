03d6ed0d719697ccbcd27eb9d213037442f11ea6
Remco Tolsma <info@remcotolsma.nl>
2015-11-19 13:44:30 +0100

    Extend from Ogone library.

--- src/Integration.php
+++ src/Integration.php
@@ -1,22 +1,10 @@
 <?php
 
-class Pronamic_WP_Pay_Gateways_Rabobank_RaboIDealKassa_Integration {
+class Pronamic_WP_Pay_Gateways_Rabobank_RaboIDealKassa_Integration extends Pronamic_WP_Pay_Gateways_Ogone_OrderStandard_Integration {
 	public function __construct() {
 		$this->id         = 'rabobank-ideal-kassa';
 		$this->name       = 'Rabobank - Rabo iDEAL Kassa';
 		$this->provider   = 'rabobank';
 		$this->deprecated = true;
-	}
-
-	public function get_config_factory_class() {
-		return 'Pronamic_WP_Pay_Gateways_Ogone_OrderStandard_ConfigFactory';
-	}
-
-	public function get_config_class() {
-		return 'Pronamic_WP_Pay_Gateways_Ogone_OrderStandard_Config';
-	}
-
-	public function get_gateway_class() {
-		return 'Pronamic_WP_Pay_Gateways_Ogone_OrderStandard_Gateway';
 	}
 }
